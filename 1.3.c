fe332b6c2ea275ec40525b6c32e35ce26cdc5d81
shahriarsnikdho <shahriarsanjidsnikdho@gmail.com>
2025-09-26 01:43:03 +0600

    Add files via upload

---
+++ 1.3.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+int main(){
+
+    int n;
+    scanf("%d", &n);
+    printf("The value of integer is: %d\n",n);
+
+    return 0;
+}
---
+++ 1.5.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main(){
+
+    float n;
+    scanf("%f", &n);
+    printf("The floating point number is: %.2f\n",n);
+
+
+    return 0;
+}
